28e16017bd048342ffc7cd0378d4eeee64333d5c
NodmGatall <oliverheidmann@hotmail.de>
2015-04-20 09:02:46 +0200

    added folders for A1 and added Task 4

---
+++ Blatt01/4/mpitest.c
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <mpi.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, size);
+	printf("Hello World from process %d of %d\n",rank,size);
+	MPI_Finalize();
+}
+return EXIT_SUCCESS;
