14d22761f9b2c7abbb4fad881c2164658de3a498
NodmGatall <oliverheidmann@hotmail.de>
2015-04-28 10:57:43 +0200

    minor changes e.g.: MPI_SEND to MPI_Send

--- Blatt02/3/simpleMPI.c
+++ Blatt02/3/simpleMPI.c
@@ -2,68 +2,75 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#ifndef
-	#define PROCESSES = 1
+#ifndef PROCESSES
+	#define PROCESSES 1
+#endif
+
+#ifdef DEBUG
+	#define debug(x,...) printf(x,##__VA_ARGS__)
+#else
+	#define debug(x,...) 
 #endif
 
 int main(int argc, char** argv)
 {
-	rc = MPI_Init(&argc ,&argv);
+	int rc = MPI_Init(&argc ,&argv);
 	if(rc != MPI_SUCCESS)
 	{
 		printf("MPI could not be initialised\n");
 		MPI_Abort(MPI_COMM_WORLD,rc);	
 	}
-	
+	debug("succes: rc = %d\n",rc);	
 	int threadID;
 	int numOfTasks;
-	int recvBuf;
+	int *recvBuf;
 	int index = 0;
 	
-	MPI_Comm_rank(MPI_Comm_World, &threadID);
-	MPI_Comm_size(MPI_Comm_World, &numOfTasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &threadID);
+	MPI_Comm_size(MPI_COMM_WORLD, &numOfTasks);
 	
+	debug("This is thread %d",threadID);
 	if(threadID == 0)
 	{
 		int rest = argc - 1 % PROCESSES;
-		int outBuf = {0,0};
+		int outBuf[2] = {0,0};
 		
 		for(;index < numOfTasks;index++)
 		{
 			for(;index < rest;index++)
 			{	
 				outBuf[1]++;
-				MPI_SEND(&outBuf,2,MPI_INT,index,0,MPI_Comm_World);
+				MPI_Send(&outBuf,2,MPI_INT,index,0,MPI_COMM_WORLD);
 				outBuf[0]++;
 
 			}
-			MPI_SEND(&outBuf,2,MPI_INT,index,0,MPI_Comm_World);
+			MPI_Send(&outBuf,2,MPI_INT,index,0,MPI_COMM_WORLD);
 		}
 		index = 0;
 		int sum = 0;
 		for(;index < numOfTasks;index++)
 		{
 			int recvBufSum = 0;
-			MPI_RECV(&recvBufSum,1,MPI_INT,index,MPI_Comm_World,MPI_STATUS_IGNORE);
+			MPI_Recv(&recvBufSum,1,MPI_INT,index,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 			printf("Recieved %d from Process %d\n",recvBufSum,index);
 			sum += recvBufSum;
 		}
 		printf("Total sum is: %d \n", sum);
 	}
-
+	debug("Proc %d is Starting\n",threadID);
 	index = 0;
 	int sectionlength = ((argc - 1) / PROCESSES);
 
-	MPI_RECV(&recvBuf,2,MPI_INT,0,MPI_Comm_World,MPI_STATUS_IGNORE);
+	MPI_Recv(&recvBuf,2,MPI_INT,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 	for(;index < PROCESSES; index++)
 	{
 		int sum = 0;
-		indexk = (index * sectionlength) + recvBuf[0];
+		int indexk = (index * sectionlength) + recvBuf[0];
 		for(;indexk < (index * sectionlength) + recvBuf[1];indexk++)
 		{
-			sum += argv[indexk+1];
+			sum += strtol(argv[indexk+1],NULL,1);
 		}
-		MPI_SEND(&sum,1,MPI_INT,0,1,MPI_Comm_World);
+		MPI_Send(&sum,1,MPI_INT,0,1,MPI_COMM_WORLD);
 	}
 
 }	
