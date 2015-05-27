65e5d0b532e1acbefdd8bf024228480e4229a2ce
NodmGatall <oliverheidmann@hotmail.de>
2015-05-27 17:52:34 +0200

    added 1.1 to 1.4

---
+++ Blatt04/1.4.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#define BILLION  1000000000L
+
+int main(int argc, char** argv)
+{
+    int index = 1;
+    int sum = 0;
+    int strAsNumber = 0;
+    struct timespec start, stop;
+    clock_gettime( CLOCK_REALTIME, &start);
+    
+    #pragma omp parralel for
+    for(;index < argc;index++)
+    {
+        sscanf(argv[index], "%d", &strAsNumber );
+    	printf("%d\n" , strAsNumber);
+	#pragma omp atomic
+        sum += strAsNumber;
+    }
+    clock_gettime( CLOCK_REALTIME, &stop);
+    printf("Sum: %d\n" , sum);
+	printf("Time: %lf\n",
+		 (double) (argc -1)/( stop.tv_sec - start.tv_sec ));
+	return EXIT_SUCCESS;
+}
